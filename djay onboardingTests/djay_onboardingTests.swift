039c3065bc8d7608ac685c29f6f8ba2e9d800fc0
Marina Lunts <marinalunts@gmail.com>
2025-04-30 14:46:59 +0200

    Initial Commit

---
+++ djay onboardingTests/djay_onboardingTests.swift
@@ -0,0 +1,17 @@
+//
+//  djay_onboardingTests.swift
+//  djay onboardingTests
+//
+//  Created by Marina Lunts on 30.04.25.
+//
+
+import Testing
+@testable import djay_onboarding
+
+struct djay_onboardingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
