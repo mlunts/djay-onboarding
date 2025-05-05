808a4b1053e017ba0c0df188dd257062c53c0655
Marina Lunts <marinalunts@gmail.com>
2025-05-05 13:44:00 +0200

    Created an onboarding step model

---
+++ djay onboarding/Models/OnboardingStep.swift
@@ -0,0 +1,13 @@
+//
+//  OnboardingStep.swift
+//  djay onboarding
+//
+//  Created by Marina Lunts on 05.05.25.
+//
+
+struct OnboardingStep {
+    let title: String
+    let description: String
+    let imageName: String?
+    let buttonTitle: String
+}
